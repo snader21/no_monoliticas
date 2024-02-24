6db5a5b2244673819a1ce36382a2d5d1516a636d
Andres Martinez <af.martinezc1@uniandes.edu.co>
2024-02-24 18:24:25 -0500

    Eventos asincronos

--- src/recopilacion/modulos/compania/aplicacion/comandos/actualizar_pais.py
+++ src/recopilacion/modulos/compania/aplicacion/comandos/actualizar_pais.py
@@ -1,11 +1,13 @@
-from src.recopilacion.modulos.compania.aplicacion.mapeadores import MapeadorCompania
 from src.recopilacion.modulos.compania.dominio.reglas import PaisesValidos
 from src.recopilacion.modulos.compania.dominio.repositorios import RepositorioCompanias
+from src.recopilacion.modulos.compania.infraestructura.schema.v1.eventos import EventoPaisActualizado, PaisActualizadoPayload
+from src.recopilacion.seedwork.infraestructura import utils
 from .base import CrearCompaniaBaseHandler
 from src.recopilacion.modulos.compania.infraestructura.dto import Compania
 from src.recopilacion.seedwork.aplicacion.comandos import Comando
 from dataclasses import dataclass, field
 from src.recopilacion.seedwork.aplicacion.comandos import ejecutar_commando as comando
+from pydispatch import dispatcher
 @dataclass
 class ActualizarPais(Comando):
     _id: str
@@ -16,12 +18,12 @@ class ActualizarPaisHandler(CrearCompaniaBaseHandler):
 
     def handle(self, comando: ActualizarPais):
         print(f"Actualizando el pais a {comando.nuevo_pais}")
-        repositorio = self.fabrica_repositorio.crear_objeto(MapeadorCompania.__class__)
+        repositorio = self.fabrica_repositorio.crear_objeto(RepositorioCompanias.__class__)
         self.fabrica_compania.validar_regla(PaisesValidos(comando.nuevo_pais))
         compania = repositorio.obtener_por_id(comando._id)
         compania.pais = comando.nuevo_pais
         repositorio.actualizar(compania)
-        
+        dispatcher.send(signal='PaisActualizadoDominio', evento=compania)
 
 
 @comando.register(ActualizarPais)
