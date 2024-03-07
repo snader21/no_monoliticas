aa3ec77b3ae1b6713853eeb76fcdceaa2b06b5a0
Ricardo Vivas <j.vivast@uniandes.edu.co>
2024-03-06 23:15:34 -0500

    add sagas structure

---
+++ sagas/src/app.py
@@ -0,0 +1,15 @@
+import os
+
+from flask_restful import Api
+from src import create_app
+
+def comenzar_consumidor():
+    import threading
+    #import src.infrastructura.consumidores as cliente
+    # Suscripción a eventos
+    #threading.Thread(target=cliente.suscribirse_a_eventos).start()
+
+app = create_app()
+app.app_context().push()
+api = Api(app)
+#comenzar_consumidor()
