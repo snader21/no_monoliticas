45920a920ddb9598038c71fc8385e0e944409820
Ricardo Vivas <j.vivast@uniandes.edu.co>
2024-03-09 22:31:55 -0500

    Add consumers

--- sagas/src/infraestructura/consumidores.py
+++ sagas/src/infraestructura/consumidores.py
@@ -4,8 +4,7 @@ import logging
 import traceback
 from src.infraestructura.util import broker_host
 from src.infraestructura.schema.v1.eventos import CompaniaCreadaPayload
-from src.aplicacion.servicios import ServicioPropiead
-from src.aplicacion.coordinadores.saga_transacciones import oir_mensaje
+from src.aplicacion.coordinadores.saga_transacciones import ejecutarComandoCompania
 from src.aplicacion.eventos.compania_creada import CompaniaCreada
 from src.app import app
 
@@ -19,12 +18,107 @@ def suscribirse_a_compania_creada():
             mensaje = consumidor.receive()
             print(f'Evento recibido compania creada: {mensaje.value()}')
             with app.app_context():
-                local_storage.getItem()
-
-                ServicioPropiead().maching_learning(mensaje.value().id_propiedad, mensaje.value().direccion)
+                ejecutarComandoCompania(mensaje.value().id_compania, mensaje.value().id_correlacion, mensaje.value().tipo)
             consumidor.acknowledge(mensaje)     
     except:
         logging.error('ERROR: Suscribiendose al tópico de compania creada!')
+        traceback.print_exc()
+        if cliente:
+            cliente.close()
+
+def suscribirse_a_compania_fallida():
+    try:
+        cliente = None
+        cliente = pulsar.Client(f'pulsar://{broker_host()}:6650')
+        consumidor = cliente.subscribe('compania_fallida', consumer_type=_pulsar.ConsumerType.Shared,subscription_name='saga-sub-eventos', schema=AvroSchema(CompaniaCreadaPayload))
+    
+        while True:
+            mensaje = consumidor.receive()
+            print(f'Evento recibido compania fallida: {mensaje.value()}')
+            with app.app_context():
+                # TODO
+                ...
+            consumidor.acknowledge(mensaje)     
+    except:
+        logging.error('ERROR: Suscribiendose al tópico de compania fallida!')
+        traceback.print_exc()
+        if cliente:
+            cliente.close()
+
+def suscribirse_a_propiedad_creada():
+    try:
+        cliente = None
+        cliente = pulsar.Client(f'pulsar://{broker_host()}:6650')
+        consumidor = cliente.subscribe('propiedad_creada', consumer_type=_pulsar.ConsumerType.Shared,subscription_name='saga-sub-eventos', schema=AvroSchema(CompaniaCreadaPayload))
+    
+        while True:
+            mensaje = consumidor.receive()
+            print(f'Evento recibido propiedad creada: {mensaje.value()}')
+            with app.app_context():
+                # TODO
+                ...
+            consumidor.acknowledge(mensaje)     
+    except:
+        logging.error('ERROR: Suscribiendose al tópico de propiedad creada!')
+        traceback.print_exc()
+        if cliente:
+            cliente.close()
+
+def suscribirse_a_propiedad_fallida():
+    try:
+        cliente = None
+        cliente = pulsar.Client(f'pulsar://{broker_host()}:6650')
+        consumidor = cliente.subscribe('propiedad_fallida', consumer_type=_pulsar.ConsumerType.Shared,subscription_name='saga-sub-eventos', schema=AvroSchema(CompaniaCreadaPayload))
+    
+        while True:
+            mensaje = consumidor.receive()
+            print(f'Evento recibido propiedad fallida: {mensaje.value()}')
+            with app.app_context():
+                # TODO
+                ...
+            consumidor.acknowledge(mensaje)     
+    except:
+        logging.error('ERROR: Suscribiendose al tópico de propiedad fallida!')
+        traceback.print_exc()
+        if cliente:
+            cliente.close()
+
+
+def suscribirse_a_transaccion_creada():
+    try:
+        cliente = None
+        cliente = pulsar.Client(f'pulsar://{broker_host()}:6650')
+        consumidor = cliente.subscribe('transaccion_creada', consumer_type=_pulsar.ConsumerType.Shared,subscription_name='saga-sub-eventos', schema=AvroSchema(CompaniaCreadaPayload))
+    
+        while True:
+            mensaje = consumidor.receive()
+            print(f'Evento recibido transaccion creada: {mensaje.value()}')
+            with app.app_context():
+                # TODO
+                ...
+            consumidor.acknowledge(mensaje)     
+    except:
+        logging.error('ERROR: Suscribiendose al tópico de transaccion creada!')
+        traceback.print_exc()
+        if cliente:
+            cliente.close()
+
+
+def suscribirse_a_transaccion_fallida():
+    try:
+        cliente = None
+        cliente = pulsar.Client(f'pulsar://{broker_host()}:6650')
+        consumidor = cliente.subscribe('transaccion_fallida', consumer_type=_pulsar.ConsumerType.Shared,subscription_name='saga-sub-eventos', schema=AvroSchema(CompaniaCreadaPayload))
+    
+        while True:
+            mensaje = consumidor.receive()
+            print(f'Evento recibido transaccion fallida: {mensaje.value()}')
+            with app.app_context():
+                # TODO
+                ...
+            consumidor.acknowledge(mensaje)     
+    except:
+        logging.error('ERROR: Suscribiendose al tópico de transaccion fallida!')
         traceback.print_exc()
         if cliente:
             cliente.close()
